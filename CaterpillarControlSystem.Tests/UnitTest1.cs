504fc021f8531c9fab29a4ff0914e3dadfc657a1
Josiah Kosgei <josiahkosgei@outlook.com>
2024-04-23 13:26:11 +0300

    Add project files.

---
+++ CaterpillarControlSystem.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CaterpillarControlSystem.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
